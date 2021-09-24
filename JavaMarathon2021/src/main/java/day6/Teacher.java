c1887d5b8e702d5babef1b8049431be4cbd442aa
Evgeniy Tatarinov <tatarinov.el@gmail.com>
2021-09-24 22:58:13 +0300

    day6 tasks3 corrected

--- JavaMarathon2021/src/main/java/day6/Teacher.java
+++ JavaMarathon2021/src/main/java/day6/Teacher.java
@@ -5,6 +5,7 @@ import java.util.Random;
 public class Teacher {
     private String name;
     private String lesson;
+    private Random rand = new Random();
 
     public Teacher(String name, String lesson) {
         this.name = name;
@@ -12,24 +13,22 @@ public class Teacher {
     }
 
     public void evaluate(Student student){
-        int mark;
-        Random rand = new Random();
-        mark = rand.nextInt(3) + 2;
-        String mark_string = "";
+        int mark = rand.nextInt(3) + 2;
+        String markString = "";
         switch (mark) {
             case 2:
-                mark_string = "неудовлетворительно";
+                markString = "неудовлетворительно";
                 break;
             case 3:
-                mark_string = "удовлетворительно";
+                markString = "удовлетворительно";
                 break;
             case 4:
-                mark_string = "хорошо";
+                markString = "хорошо";
                 break;
             case 5:
-                mark_string = "отлично";
+                markString = "отлично";
                 break;
         }
-        System.out.println("Преподаватель " + name + " оценил студента с именем " + student.getName() + " по предмету " + lesson + " на оценку " + mark_string + ".");
+        System.out.println("Преподаватель " + name + " оценил студента с именем " + student.getName() + " по предмету " + lesson + " на оценку " + markString + ".");
     }
 }
