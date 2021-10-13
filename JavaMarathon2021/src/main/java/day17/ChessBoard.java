f54495e2cc5b57655bbf829f7cd5063604b78140
Evgeniy Tatarinov <tatarinov.el@gmail.com>
2021-10-13 23:39:41 +0300

    Day17 tasks2 modified

--- JavaMarathon2021/src/main/java/day17/ChessBoard.java
+++ JavaMarathon2021/src/main/java/day17/ChessBoard.java
@@ -5,8 +5,19 @@ import java.util.Arrays;
 public class ChessBoard {
     private ChessPiece[][] board;
 
-    public ChessBoard(ChessPiece[][] board) {
-        this.board = board;
+//    public ChessBoard(ChessPiece[][] board) {
+//    this.board = board;
+    public ChessBoard(int lineCount, int columnCount) {
+        board = new ChessPiece[lineCount][columnCount];
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+                board[i][j] = ChessPiece.EMPTY;
+            }
+        }
+    }
+
+    public void setChessPies ( ChessPiece chessPies, int line, int column){
+        board[line][column] = chessPies;
     }
 
     public void print() {
--- JavaMarathon2021/src/main/java/day17/Task2.java
+++ JavaMarathon2021/src/main/java/day17/Task2.java
@@ -2,16 +2,26 @@ package day17;
 
 public class Task2 {
     public static void main(String[] args) {
-        ChessBoard chessBoard = new ChessBoard(new ChessPiece[][]{
-                {ChessPiece.ROOK_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_BLACK, ChessPiece.KING_BLACK, ChessPiece.EMPTY},
-                {ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK},
-                {ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.KNIGHT_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY},
-                {ChessPiece.QUEEN_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
-                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_BLACK, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
-                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY},
-                {ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.QUEEN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE},
-                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.KING_WHITE, ChessPiece.EMPTY}
-        });
+//        ChessBoard chessBoard = new ChessBoard(new ChessPiece[][]{
+//                {ChessPiece.ROOK_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_BLACK, ChessPiece.KING_BLACK, ChessPiece.EMPTY},
+//                {ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK},
+//                {ChessPiece.PAWN_BLACK, ChessPiece.EMPTY, ChessPiece.KNIGHT_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.PAWN_BLACK, ChessPiece.EMPTY},
+//                {ChessPiece.QUEEN_BLACK, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
+//                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_BLACK, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY},
+//                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.BISHOP_WHITE, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY},
+//                {ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.QUEEN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE, ChessPiece.EMPTY, ChessPiece.PAWN_WHITE},
+//                {ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.EMPTY, ChessPiece.ROOK_WHITE, ChessPiece.KING_WHITE, ChessPiece.EMPTY}
+//        });
+
+        ChessBoard chessBoard = new ChessBoard(7, 7);
+        chessBoard.setChessPies(ChessPiece.ROOK_BLACK, 0, 0);
+        chessBoard.setChessPies(ChessPiece.ROOK_BLACK, 0, 5);
+        chessBoard.setChessPies(ChessPiece.KING_BLACK, 0, 6);
+        chessBoard.setChessPies(ChessPiece.ROOK_WHITE, 1, 1);
+        chessBoard.setChessPies(ChessPiece.PAWN_BLACK, 1, 4);
+        chessBoard.setChessPies(ChessPiece.PAWN_BLACK, 1, 5);
+        // etc.
+
         chessBoard.print();
     }
 }
